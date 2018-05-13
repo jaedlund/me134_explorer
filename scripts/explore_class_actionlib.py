0f29db8ffc07176d96a883921892b875339d4486
Jeffrey Edlund and Melissa Tanner <me134tas@robotics.caltech.edu>
2018-05-13 14:53:49 -0700

    Plots the frontiers,walls, and robot dot.

--- scripts/explore_class_actionlib.py
+++ scripts/explore_class_actionlib.py
@@ -17,7 +17,7 @@ import ros_numpy
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-
+import numpy
 
 class ME134_Explorer:
     def __init__(self):
@@ -34,16 +34,17 @@ class ME134_Explorer:
         rospy.init_node('me134_explorer', anonymous=False)
         #rospy.Subscriber("amcl_pose", PoseWithCovarianceStamped, self.poseCallback)
         rospy.Subscriber("map", OccupancyGrid, self.mapCallback)
-        rospy.Subscriber("map_metadata", MapMetaData, self.mapMetaDataCallback)
+        #rospy.Subscriber("map_metadata", MapMetaData, self.mapMetaDataCallback)
         #rospy.Subscriber("scan", LaserScan, self.scanCallback)
         #rospy.Subscriber("move_base/status",GoalStatusArray, self.goalStatusCallback)
         
         self.pub_goal = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=10)
         rospy.loginfo('explorer online')
+        self.overlay_colors={1:(0,255,0)}
         pass
 
     def CheckIfHaveFirstData(self):
-        return self.last_map and self.last_map_metadata #and self.last_pose
+        return self.last_map # and self.last_map_metadata #and self.last_pose
 
     def AddInplaceRotationsToQueue(self):
         import math
@@ -68,7 +69,8 @@ class ME134_Explorer:
         #rospy.loginfo(rospy.get_caller_id()+"map received: {}".format(occupancyGridData))
         #rospy.loginfo(rospy.get_caller_id()+" map received.")
         self.last_map = occupancyGridData
-        self.last_map_numpy = ros_numpy.occupancy_grid.occupancygrid_to_numpy(self.last_map)
+        # occupancygrid_to_numpy returns a masked numpy array, we use the .data to remove the mask.
+        self.last_map_numpy = ros_numpy.occupancy_grid.occupancygrid_to_numpy(self.last_map).data
         #self.last_map_from_above = numpy.flipud(self.last_map_numpy)
         
         resolution = self.last_map.info.resolution
@@ -80,24 +82,31 @@ class ME134_Explorer:
 
         pass
 
-    def PlotMap(self):
+    def PlotMap(self,overlay=None,overlay_colors=None):
         assert self.last_map
         import matplotlib.pyplot as plt
                 
         #plt.imshow(self.last_map_from_above,extent=self.last_map_from_above_extents)
         fig,ax = plt.subplots()
-    
-        ax.imshow(self.last_map_numpy,extent=self.last_map_extents,origin='lower')
+        m = self.last_map_numpy
+        image = numpy.zeros((self.last_map_numpy.shape[0],self.last_map_numpy.shape[1],3),dtype=numpy.uint8)
+        print set(m.flatten().tolist())
+        image[m==-1] = (150,150,150)
+        image[m==100] = (0,0,0)
+        image[m==0] = (255,255,255)
+        if overlay is not None:
+            assert overlay_colors
+            for value,color in overlay_colors.iteritems():
+                image[overlay==value] = color
+                pass
+            pass
+        ax.imshow(image,extent=self.last_map_extents,origin='lower')
+        ax.autoscale(tight=True)
         #x,y,yaw= self.Get_x_y_yaw()
         x,y,yaw= 0,0,0
-        ax.plot(x,y,'o')
+        ax.plot(x,y,'o') # TODO: 
         plt.show()
-        
-    def mapMetaDataCallback(self,mapMetaData):
-        #rospy.loginfo(rospy.get_caller_id()+"map received: {}".format(occupancyGridData))
-        #rospy.loginfo(rospy.get_caller_id()+" map received.")
-        self.last_map_metadata = mapMetaData
-        pass
+        1/0
 
     # def scanCallback(self,scanData):
     #     #rospy.loginfo(rospy.get_caller_id()+"map received: {}".format(occupancyGridData))
@@ -129,12 +138,44 @@ class ME134_Explorer:
         else:
             return client.get_result()
         pass
+
+    def FindFrontier(self,m):
+        free_space_indices = numpy.where(m==0)
+        print "free_space_indices=",free_space_indices
+        # Brute force way
+        frontier_indices = []
+        for (a,b) in zip(*free_space_indices):
+            for off_a in (-1,0,1):
+                for off_b in (-1,0,1):
+                    if off_a == 0 and off_b == 0:
+                        continue
+                    a_new,b_new = a+off_a,b+off_b
+                    if 0<=a_new<m.shape[0] and 0<=b_new<m.shape[1]:
+                        if m[a_new,b_new] == -1:
+                            frontier_indices.append((a,b))
+                            pass
+                        pass
+                    else:
+                        print "WARNING: free space at map edge"
+                        pass
+                    pass
+                pass
+            pass
+        return frontier_indices
+
+    def ProcessMap(self):
+        self.frontier_indices = self.FindFrontier(self.last_map_numpy)
+        self.overlay = numpy.zeros(self.last_map_numpy.shape)
+        for (a,b) in self.frontier_indices:
+            self.overlay[a,b]=1
+            pass
         
 
     def Step(self):
         print "Mode=",self.mode
         if self.CheckIfHaveFirstData():
-            self.PlotMap()
+            self.ProcessMap()
+            self.PlotMap(overlay=self.overlay,overlay_colors=self.overlay_colors)
             if self.mode is None:
                 self.goal_queue.append((0,0,0))
                 self.AddInplaceRotationsToQueue()
