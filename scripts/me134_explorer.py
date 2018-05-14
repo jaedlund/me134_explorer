2cca48d5372477ed89558a76201a6ebaa22a02f0
Melissa Tanner <mmtanner@caltech.edu>
2018-05-14 14:56:07 -0700

    Added comment

--- scripts/me134_explorer.py
+++ scripts/me134_explorer.py
@@ -63,12 +63,13 @@ class ME134_Explorer:
         
         #self.pub_goal = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=10)
         rospy.loginfo('explorer online')
-        self.overlay_colors={1:(0,255,0),}
+        self.overlay_colors={1:(0,255,0),} # frontier color green
         pass
 
     def CheckIfHaveFirstData(self):
         return self.last_map and self.last_pose and self.last_global_costmap #and self.last_map_metadata 
 
+    # This works, but you might find a better way
     def AddInplaceRotationsToQueue(self):
         x,y,yaw = self.last_pose
         for yaw_target in [math.pi/2,math.pi,-math.pi/2,0]:
@@ -126,7 +127,7 @@ class ME134_Explorer:
         right = left + occupancyGridData.info.width*resolution
         bottom = occupancyGridData.info.origin.position.y
         top = bottom + occupancyGridData.info.height*resolution
-        self.last_map_extents = (left,right,bottom,top) 
+        self.last_map_extents = (left,right,bottom,top) # for plotting
 
         pass
 
@@ -155,10 +156,12 @@ class ME134_Explorer:
         fig,ax = plt.subplots()
         m = map_data
         image = numpy.zeros((m.shape[0],m.shape[1],3),dtype=numpy.uint8)
-        print set(m.flatten().tolist())
+        # useful for debugging. Because numbers besides -1,0,or 100 won't show up on the map.
+        #print set(m.flatten().tolist()) 
         image[m==-1] = (150,150,150)
         image[m==100] = (0,0,0)
         image[m==0] = (255,255,255)
+        # you can set your own overlay colors
         if overlay is not None:
             assert overlay_colors
             for value,color in overlay_colors.iteritems():
@@ -170,18 +173,18 @@ class ME134_Explorer:
         ax.set_xlabel("x position (m)")
         ax.set_ylabel("y position (m)")
         ax.set_title("map")
-        #x,y,yaw= self.Get_x_y_yaw()
         x,y,yaw= self.last_pose
         ax.plot(x,y,'o', label="robot") # TODO: plot robot pointing direction 
-        if self.goal_queue:
+        if self.goal_queue: # If non-empty goal queue, plot first goal in the queue
             x,y,yaw= self.goal_queue[0]
             ax.plot(x,y,'x',label="next goal")
             pass
         ax.legend(loc='best', fancybox=True, framealpha=0.5)
 
         return fig
-    
-    def PlotCostMap(self,overlay=None,overlay_colors=None):
+
+    # plots the most recent costmap (from subscribed topic)
+    def PlotCostMap(self):
         assert self.last_global_costmap
                 
         #plt.imshow(self.last_map_from_above,extent=self.last_map_from_above_extents)
@@ -234,15 +237,16 @@ class ME134_Explorer:
             return client.get_result()
         pass
 
+    # returns indices of frontiers
     def FindFrontier(self,m):
         free_space_indices = numpy.where(m==0)
         #print "free_space_indices=",free_space_indices
         # Brute force way
         frontier_indices = []
-        for (ia,ib) in zip(*free_space_indices):
+        for (ia,ib) in zip(*free_space_indices): # iterates through all free spaces
             has_unknown=False
             has_free=False
-            for off_ia,off_ib in ((-1,0),(1,0),(0,-1),(0,1)):
+            for off_ia,off_ib in ((-1,0),(1,0),(0,-1),(0,1)): # frontier is a free space with at least one non-diagonal neighbor unknown and one non-diagonal neighbor free
                 if has_unknown and has_free:
                     break
                 ia_new,ib_new = ia+off_ia,ib+off_ib
@@ -271,10 +275,13 @@ class ME134_Explorer:
         y = info.origin.position.y + ia*resolution
         return x,y
 
+    # find the closest point on a frontier that is at least min_required_distance away
+    # NOTE: If you are writing your own exploration algorithm, you will probably want to replace this method with your own
     def FindClosestFrontier(self,min_required_distance=None):
         rx,ry,r_yaw = self.last_pose
         min_distance = None
         target_x_y = None
+        # Brute force approach: convert all frontier indices to distances
         for ia,ib in self.frontier_indices:
             tx,ty = self.ConvertMapIndicesToMeters(ia,ib)
             distance = math.sqrt((rx-tx)**2+(ry-ty)**2)
@@ -290,12 +297,12 @@ class ME134_Explorer:
         return target_x_y
 
     
-    
+    # builds frontiers and makes overlay (to plot frontiers on top of map)
     def ProcessMap(self):
         self.frontier_indices = self.FindFrontier(self.last_map_numpy)
         self.overlay = numpy.zeros(self.last_map_numpy.shape)
-        for (x,y) in self.frontier_indices:
-            self.overlay[x,y]=1
+        for (ia,ib) in self.frontier_indices:
+            self.overlay[ia,ib]=1 # mark as frontier
             pass
         
 
@@ -306,18 +313,19 @@ class ME134_Explorer:
         if self.CheckIfHaveFirstData():
             self.ProcessMap()
             print "goal_queue={}".format(self.goal_queue)
-            if plotEachStep and (self.mode not in ["Rotating","Initial Movement"]):
+            if plotEachStep and (self.mode not in ["Rotating","Initial Movement"]): # self.mode - FSM markign current state
                 fig1=self.PlotCostMap()
                 fig2=self.PlotMap(overlay=self.overlay,overlay_colors=self.overlay_colors)
-                plt.show()
-                plt.close(fig1)
+                plt.show()  # have to close windows for program to continue
+                plt.close(fig1) # free figures so as to not run out of memory
                 plt.close(fig2)
-            if self.goal_queue:
+            if self.goal_queue: # if there's anythign in the goal queue, do it
                 x,y,yaw = self.goal_queue.pop(0)
                 self.PublishGoal(x,y,yaw)
                 pass
-            else:
+            else: # once done with goal queue, move on to the next step, given by self.next_mode
                 # Change the following code to run your algorithm
+                # *------------------------------------------------*
                 self.mode = self.next_mode
                 
                 if self.mode is None: # in the beginning
@@ -325,6 +333,8 @@ class ME134_Explorer:
                     # There is a bug https://answers.ros.org/question/204740/dwa_local_planner-in-place-rotation-first-goal-does-not-work/
                     # That means that the first movement can't be a turn in place
                     # If we reverse a bit first the robot will rotate to clear the costmap
+                    # After that, you can move robot in any way you want
+                    # TODO: maybe you can find a better way to do this
 
                     self.AddMoveForwardToQueue(-0.25)
                     # Then return to the starting place
@@ -337,13 +347,16 @@ class ME134_Explorer:
                     self.next_mode = "Rotating"
                     # You should find a position on the map that views a frontier
                     # from a safe distance using the global_cost_map and/or
-                    # another algorithm. I'm only going to go to the closest one
+                    # another algorithm. Warning: global costmap doesn't always update regularly. 
+                    # You can probably fix this if you want by subscribing to global_costmap_updates. 
+                    # I'm only going to attempt to go to the closest one
                     # (probably not safe) for the demo.
-                    target_x_y = self.FindClosestFrontier(min_required_distance=0.1)
+                    target_x_y = self.FindClosestFrontier(min_required_distance=0.1) # otherwise robot will try to go to the  frontier directly under the robot - at least at first, the laser scanner won't scan that!
                     print("WARNING: Unless you change this code it won't drive anywhere. The goal is too close to a wall. You need to find a place to safely view this spot.")
                     
                     if target_x_y is not None:
                         tx,ty=target_x_y
+                        # we're setting yaw=0 here. You may want to compute yaw angle, or choose it carefully...
                         self.goal_queue.append((tx,ty,0))
                         pass
                     else:
@@ -357,6 +370,7 @@ class ME134_Explorer:
                 else:
                     print "Unknown mode={}".format(self.mode)
                     return True
+                # *-------------------------------------------------------------*
                 pass
             pass
         else:
