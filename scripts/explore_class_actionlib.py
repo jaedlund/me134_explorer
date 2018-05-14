ffb69f91405a8f64f0c579a9fb43e70bab242b97
Jeffrey Edlund and Melissa Tanner <me134tas@robotics.caltech.edu>
2018-05-13 17:52:02 -0700

    Testing that I have the coordinates right and there's still a problem with an unexplored area under the robot.

--- scripts/explore_class_actionlib.py
+++ scripts/explore_class_actionlib.py
@@ -19,6 +19,9 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 #from std_msgs.msg import Int8MultiArray
 import numpy
+import math
+import matplotlib.pyplot as plt
+
 
 class ME134_Explorer:
     def __init__(self):
@@ -60,20 +63,15 @@ class ME134_Explorer:
         #self.pub_goal = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=10)
         rospy.loginfo('explorer online')
         self.overlay_colors={1:(0,255,0),}
-        #image[m==-1] = (150,150,150)
-        #image[m==100] = (0,0,0)
-        #image[m==0] = (255,255,255)
         pass
 
     def CheckIfHaveFirstData(self):
         return self.last_map and self.last_pose and self.last_global_costmap #and self.last_map_metadata 
 
     def AddInplaceRotationsToQueue(self):
-        import math
-        import numpy
         x,y,yaw = self.last_pose
-        for yaw_target in [math.pi/2,math.pi,-math.pi/2,0]:#numpy.linspace(yaw,yaw-2*math.pi,10):
-            self.goal_queue.append((x,y,yaw+yaw_target))
+        for yaw_target in [math.pi/2,math.pi,-math.pi/2,0]:
+            self.goal_queue.append((x,y,yaw_target))
             pass
         pass
     
@@ -129,7 +127,7 @@ class ME134_Explorer:
         #rospy.loginfo(rospy.get_caller_id()+" map received.")
         self.last_global_costmap = occupancyGridData
         # occupancygrid_to_numpy returns a masked numpy array, we use the .data to remove the mask.
-        self.last_global_costmap_numpy = ros_numpy.occupancy_grid.occupancygrid_to_numpy(occupancyGridData).data
+        self.last_global_costmap_numpy = ros_numpy.occupancy_grid.occupancygrid_to_numpy(occupancyGridData)
         #self.last_map_from_above = numpy.flipud(self.last_map_numpy)
         
         resolution = occupancyGridData.info.resolution
@@ -140,17 +138,12 @@ class ME134_Explorer:
         self.last_global_costmap_extents = (left,right,bottom,top)
     
         pass
-    
-    
-    
-    def PlotMap(self, overlay=None, overlay_colors=None):
-        self.PlotGeneralMap(self.last_map_numpy,self.last_map_extents,overlay=overlay,overlay_colors=overlay_colors)
-        pass
-    
-    def PlotGeneralMap(self,map_data,map_extents,overlay=None,overlay_colors=None):
+        
+    def PlotMap(self,overlay=None,overlay_colors=None):
         assert self.last_map
-        import matplotlib.pyplot as plt
-                
+
+        map_data = self.last_map_numpy
+        map_extents = self.last_map_extents
         #plt.imshow(self.last_map_from_above,extent=self.last_map_from_above_extents)
         fig,ax = plt.subplots()
         m = map_data
@@ -165,41 +158,43 @@ class ME134_Explorer:
                 image[overlay==value] = color
                 pass
             pass
-        ax.imshow(image,extent=map_extents,origin='lower')
+        ax.imshow(image,extent=map_extents,origin='lower',interpolation='none')
         ax.autoscale(tight=True)
+        ax.set_xlabel("x position (m)")
+        ax.set_ylabel("y position (m)")
+        ax.set_title("map")
         #x,y,yaw= self.Get_x_y_yaw()
         x,y,yaw= self.last_pose
-        ax.plot(x,y,'o') # TODO: plot robot pointing direction 
-        plt.show()
-    
-    
+        ax.plot(x,y,'o', label="robot") # TODO: plot robot pointing direction 
+        if self.goal_queue:
+            x,y,yaw= self.goal_queue[0]
+            ax.plot(x,y,'x',label="next goal")
+            pass
+        ax.legend(loc='best', fancybox=True, framealpha=0.5)
+
+        return fig
     
     def PlotCostMap(self,overlay=None,overlay_colors=None):
-        assert self.last_map
-        import matplotlib.pyplot as plt
+        assert self.last_global_costmap
                 
         #plt.imshow(self.last_map_from_above,extent=self.last_map_from_above_extents)
         fig,ax = plt.subplots()
         m = self.last_global_costmap_numpy
         m_extents = self.last_global_costmap_extents
-        #image = numpy.zeros((self.last_map_numpy.shape[0],self.last_map_numpy.shape[1],3),dtype=numpy.uint8)
-        #print set(m.flatten().tolist())
-        #image[m==-1] = (150,150,150)
-        #image[m==100] = (0,0,0)
-        #image[m==0] = (255,255,255)
-        #if overlay is not None:
-        #    assert overlay_colors
-        #    for value,color in overlay_colors.iteritems():
-        #        image[overlay==value] = color
-        #        pass
-        #    pass
-        ax.imshow(m,extent=m_extents,origin='lower')
+        ax.imshow(m,extent=m_extents,origin='lower',cmap='Oranges',interpolation='none')
         ax.autoscale(tight=True)
+        ax.set_xlabel("x position (m)")
+        ax.set_ylabel("y position (m)")
+        ax.set_title("global cost map")
         #x,y,yaw= self.Get_x_y_yaw()
-        x,y,yaw= 0,0,0
-        ax.plot(x,y,'o') # TODO: 
-        plt.show()
-        1/0
+        x,y,yaw= self.last_pose
+        ax.plot(x,y,'o',label="robot") 
+        if self.goal_queue:
+            x,y,yaw= self.goal_queue[0]
+            ax.plot(x,y,'x',label="next goal")
+            pass
+        ax.legend(loc='best', fancybox=True, framealpha=0.5)
+        return fig
 
     # def scanCallback(self,scanData):
     #     #rospy.loginfo(rospy.get_caller_id()+"map received: {}".format(occupancyGridData))
@@ -234,18 +229,18 @@ class ME134_Explorer:
 
     def FindFrontier(self,m):
         free_space_indices = numpy.where(m==0)
-        print "free_space_indices=",free_space_indices
+        #print "free_space_indices=",free_space_indices
         # Brute force way
         frontier_indices = []
-        for (a,b) in zip(*free_space_indices):
-            for off_a in (-1,0,1):
-                for off_b in (-1,0,1):
-                    if off_a == 0 and off_b == 0:
+        for (ia,ib) in zip(*free_space_indices):
+            for off_ia in (-1,0,1):
+                for off_ib in (-1,0,1):
+                    if off_ia == 0 and off_ib == 0:
                         continue
-                    a_new,b_new = a+off_a,b+off_b
-                    if 0<=a_new<m.shape[0] and 0<=b_new<m.shape[1]:
-                        if m[a_new,b_new] == -1:
-                            frontier_indices.append((a,b))
+                    ia_new,ib_new = ia+off_ia,ib+off_ib
+                    if 0<=ia_new<m.shape[0] and 0<=ib_new<m.shape[1]:
+                        if m[ia_new,ib_new] == -1:
+                            frontier_indices.append((ia,ib))
                             pass
                         pass
                     else:
@@ -256,35 +251,90 @@ class ME134_Explorer:
             pass
         return frontier_indices
 
+    def ConvertMapIndicesToMeters(self,ia,ib):
+        info = self.last_map.info
+        resolution = info.resolution
+        x = info.origin.position.x + ib*resolution
+        y = info.origin.position.y + ia*resolution
+        return x,y
+
+    def FindClosestFrontier(self,min_required_distance=None):
+        rx,ry,r_yaw = self.last_pose
+        min_distance = None
+        target_x_y = None
+        for ia,ib in self.frontier_indices:
+            tx,ty = self.ConvertMapIndicesToMeters(ia,ib)
+            distance = math.sqrt((rx-tx)**2+(ry-ty)**2)
+            if min_required_distance is not None and distance < min_required_distance:
+                print "Skipping point {} with distance {} which might be under the robot".format((tx,ty),distance)
+                continue
+            if min_distance is None or distance < min_distance:
+                min_distance = distance
+                target_x_y = (tx,ty)
+                pass
+            pass
+        print "Closest Frontier is at {}. It is {} meters from the robot.".format(target_x_y,min_distance)
+        return target_x_y
+    
     def ProcessMap(self):
         self.frontier_indices = self.FindFrontier(self.last_map_numpy)
         self.overlay = numpy.zeros(self.last_map_numpy.shape)
-        for (a,b) in self.frontier_indices:
-            self.overlay[a,b]=1
+        for (x,y) in self.frontier_indices:
+            self.overlay[x,y]=1
             pass
         
 
     def Step(self):
         print "Mode=",self.mode
-
+        plotEachStep=True
         self.getLastPose()
         if self.CheckIfHaveFirstData():
             self.ProcessMap()
-            self.PlotCostMap()
-            self.PlotMap(overlay=self.overlay,overlay_colors=self.overlay_colors)
-            if self.mode is None: # in the beginning
-                #self.goal_queue.append((0,0,0))
-                self.AddInplaceRotationsToQueue()
-                self.mode = "Rotating"
-                x,y,yaw = self.goal_queue.pop(0)
-                self.PublishGoal(x,y,yaw)
-                pass
+            print "goal_queue={}".format(self.goal_queue)
+            if plotEachStep and self.mode != "Rotating":
+                fig1=self.PlotCostMap()
+                fig2=self.PlotMap(overlay=self.overlay,overlay_colors=self.overlay_colors)
+                plt.show()
+                plt.close(fig1)
+                plt.close(fig2)
             if self.goal_queue:
                 x,y,yaw = self.goal_queue.pop(0)
                 self.PublishGoal(x,y,yaw)
                 pass
             else:
-                return True
+                # Change the following code to run your algorithm
+            
+                if self.mode is None: # in the beginning
+                    #self.goal_queue.append((0,0,0))
+                    self.AddInplaceRotationsToQueue()
+                    self.mode = "Rotating"
+                    pass
+                elif self.mode == "Rotating":
+                    #Done rotating
+                    
+                    self.mode = "find_frontier"
+                    # You should find a position on the map that views a frontier
+                    # from a safe distance using the global_cost_map and/or
+                    # another algorithm. I'm only going to go to the closest one
+                    # (probably not safe) for the demo.
+                    target_x_y = self.FindClosestFrontier(min_required_distance=0.1)
+                    if target_x_y is not None:
+                        tx,ty=target_x_y
+                        self.goal_queue.append((tx,ty,0))
+                        pass
+                    else:
+                        print "No Frontiers found"
+                        self.abort = True
+                        pass
+                elif self.mode == "find_frontier":
+                    print "Rotating again"
+                    self.AddInplaceRotationsToQueue()
+                    self.mode = "Rotating"
+                    pass
+                else:
+                    print "Unknown mode={}".format(self.mode)
+                    return True
+                pass
             pass
         else:
             print "NoMapScan"
